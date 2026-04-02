a5a95eb882612819c3aafcc0fa39bad85654fafc
ketevan20 <ketevansamukashvili@gmail.com>
2026-04-02 14:16:00 +0400

    feat: add error state when API request fails

--- app/components/CurrentWeather.tsx
+++ app/components/CurrentWeather.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const CurrentWeather = () => {
+type CurrentWeatherProps = {
+  current: any;
+  timezone: string;
+  time?: string;
+}
+
+const CurrentWeather = ({current, timezone, time}: CurrentWeatherProps) => {
   return (
-    <div>CurrentWeather</div>
+    <div className='w-full h-full bg-[url("/images/bg-today-large.svg")] bg-no-repeat bg-cover rounded-[20px] px-6 py-20 flex items-center justify-between text-white'>
+      <div className='flex flex-col gap-3'>
+        <h2 className='h2'>{timezone}</h2>
+        <p>{time}</p>
+      </div>
+      <div className='flex items-center'>
+        <img src="/images/icon-sunny.webp" alt="weather icon" className='w-30 h-30 object-cover'/>
+        <h1 className='h1'>{current?.temperature_2m}°</h1>
+      </div>
+    </div>
   )
 }
 
